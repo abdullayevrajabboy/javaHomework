9772bb0b9954bab561a297ce7fa7499a20a45565
rajabboy <a.rajabboy756@gmail.com>
2023-10-11 17:13:43 +0500

    homework

---
+++ homework1/src/main/java/Uchburchak.java
@@ -0,0 +1,20 @@
+public class Uchburchak {
+    float katet1;
+    float katet2;
+    float gipotenuza;
+
+    public Uchburchak(float katet1, float katet2, float gipotenuza) {
+        this.katet1 = katet1;
+        this.katet2 = katet2;
+        this.gipotenuza = gipotenuza;
+    }
+
+    void Information(){
+        System.out.println("birinchi tomon:"+this.katet1+", ikkinchi tomon:"+this.katet2+",gipotenuza:"+this.gipotenuza);
+    }
+
+    public static void main(String[] args) {
+        Uchburchak uchburchak=new Uchburchak(3.0F, 4.0F, 5.0F);
+        uchburchak.Information();
+    }
+}
